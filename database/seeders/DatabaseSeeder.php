e564b7996bb4ca4f8223e067a1ac4509d5cd540a
Ensar Aydin Kurubacak <eaydin.kurubacak@gmail.com>
2023-07-22 18:28:20 +0300

    Some changes maden in DatabaseSeeder class. We do not just insert admin record to the user table anymore but we also create one instance of user model. With the help of this newly created user instance, we can check an existence of this user (or admin) in login page.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
@@ -20,10 +21,19 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
+        /* Not working
         DB::table('users')->insert([
             'name' => 'admin',
             'email' => 'admin@admin.com',
             'password' => 'password',
+        ]);
+        */
+
+        // Working
+        $user = User::create([
+            'name' => 'admin',
+            'email' => 'admin@admin.com',
+            'password' => bcrypt('password'),
         ]);
     }
 }
